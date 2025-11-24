d07a545050591a7de46e141842456eccfb1d7c80
Robert Pallesen <Robert.pallesen13@gmail.com>
2025-11-24 16:07:50 +0100

    add todo

--- src/main/java/SPAC/MealFlow/FullstackApplication.java
+++ src/main/java/SPAC/MealFlow/FullstackApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FullstackApplication {
 
+
+    // TODO:
+    // Creating recipe needs ingredients to be fetched from the database
+    // Create a populate DB script to add some ingredients, users and recipes to the DB at start up
 	public static void main(String[] args) {
 		SpringApplication.run(FullstackApplication.class, args);
 	}
