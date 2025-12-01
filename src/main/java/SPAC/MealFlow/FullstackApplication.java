d2a28e994e12bbb3e684076ab1544fc096a7f35f
Robert Pallesen <Robert.pallesen13@gmail.com>
2025-12-01 14:56:41 +0100

    updated todo

--- src/main/java/SPAC/MealFlow/FullstackApplication.java
+++ src/main/java/SPAC/MealFlow/FullstackApplication.java
@@ -10,6 +10,10 @@ public class FullstackApplication {
     // TODO:
     // Creating recipe needs ingredients to be fetched from the database
     // Create a populate DB script to add some ingredients, users and recipes to the DB at start up
+
+
+    // Make so serving/size can be altered
+    // add week day to mealplan
 	public static void main(String[] args) {
 		SpringApplication.run(FullstackApplication.class, args);
 	}
